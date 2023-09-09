f0010eeab0c03d11709f7277d28b741a0b1c9595
Rodrigo Diaz de Leon C <rodrigocimp@outlook.com>
2023-09-08 21:05:49 -0500

    Primeras soluciones CodeWars

---
+++ CodeWars/Basic.py
@@ -0,0 +1,13 @@
+#Array plus array - 8 kyu
+def array_plus_array(arr1,arr2):
+    return sum(arr1)+sum(arr2)
+
+
+#Vexing Vanishing Values - 8 kyu
+def mul_by_n(lst: list, n: int) -> list:
+    return [i * n for i in lst]
+
+
+#FIXME: Replace all dots - 8 kyu
+def replace_dots(str):
+    return str.replace(".", "-")
