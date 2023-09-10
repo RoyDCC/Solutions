52ef2de9473a4862bb2a528e47af7871d69dc489
Rodrigo Diaz de Leon C <rodrigocimp@outlook.com>
2023-09-10 12:22:44 -0500

    Descripciones problemas 6+ (Solo Python)

--- python/MovingZerosToTheEnd.py
+++ python/MovingZerosToTheEnd.py
@@ -1,5 +1,13 @@
 # Moving Zeros To The End - 5 kyu
 
+'''
+Write an algorithm that takes an array and moves all of the zeros to the end,
+    preserving the order of the other elements.
+
+Example:
+    moveZeros([false,1,0,1,2,0,1,3,"a"]) // returns[false,1,1,2,1,3,"a",0,0]
+'''
+
 def move_zeros(arr): # 1st Solution
     zero = True
     count = 0
