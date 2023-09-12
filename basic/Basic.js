68e7487215d34247edd5871c9988fd67d5f85def
Rodrigo Diaz de Leon C <rodrigocimp@outlook.com>
2023-09-11 21:32:54 -0500

    Problemas js

--- basic/Basic.js
+++ basic/Basic.js
@@ -1,4 +1,13 @@
-// Multiply
+// Multiply - 8 kyu
 function multiply(a, b){
     return a * b
+}
+
+//Sum of positives - 8 kyu
+function positiveSum(arr) {
+    let sum = 0;
+    arr.forEach(n => {
+        if (n>0) sum+=n;
+    });
+    return sum;
 }
