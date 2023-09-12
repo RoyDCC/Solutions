ea35481d2dcf92c8ed2c655d60de644e6571e109
Rodrigo Diaz de Leon C <rodrigocimp@outlook.com>
2023-09-11 21:52:41 -0500

    1 Problema js

--- basic/Basic.js
+++ basic/Basic.js
@@ -11,3 +11,16 @@ function positiveSum(arr) {
     });
     return sum;
 }
+
+//Check same case - 8 kyu
+function sameCase(a, b){
+    let upper = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+    let lower = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+    if ((upper.includes(a) && upper.includes(b)) || (lower.includes(a) && lower.includes(b))){
+        return 1;
+    }else if ((upper.includes(a) || lower.includes(a)) && (upper.includes(b) || lower.includes(b))){
+        return 0;
+    }else{
+        return -1;
+    }
+}
