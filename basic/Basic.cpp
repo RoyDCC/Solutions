be5d786cc865c8b6829fab1e1a6e3277f1f05cd4
Rodrigo Diaz de Leon C <rodrigocimp@outlook.com>
2023-09-25 20:45:55 -0600

    Problema Basic.cpp code fast

--- basic/Basic.cpp
+++ basic/Basic.cpp
@@ -1,8 +1,13 @@
 // Jenny's secret message - 8 kyu
-std::string greet(std::string name) 
-{
+std::string greet(std::string name) {
   if(name == "Johnny") {
     return "Hello, my love!";
   }
   return "Hello, " + name + "!";
+}
+
+// You Can't Code Under Pressure #1 - 8 kyu
+#include <cstdint>
+int32_t double_integer(int32_t n){
+  return n*2;
 }
