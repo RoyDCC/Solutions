445ffde82aaab12580e0b6ad1624c57fe6122682
RoyDCC <rodrigo.diazdeleonc@gmail.com>
2023-11-08 21:07:07 -0600

    Problem cpp 8k planet

--- basic/Basic.cpp
+++ basic/Basic.cpp
@@ -11,3 +11,22 @@ std::string greet(std::string name) {
 int32_t double_integer(int32_t n){
   return n*2;
 }
+
+
+// Get Planet Name By ID - 8 kyu
+#include <string>
+std::string get_planet_name(int id)
+{
+    std::string name;
+    switch (id) {
+    case 1: name = "Mercury"; break;
+    case 2: name = "Venus";break;
+    case 3: name = "Earth";break;
+    case 4: name = "Mars";break;
+    case 5: name = "Jupiter";break;
+    case 6: name = "Saturn";break;
+    case 7: name = "Uranus";break;
+    case 8: name = "Neptune";
+    }
+    return name;
+}
